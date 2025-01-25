878ce90d5b08b90e51cb260402db2bd840266a02
Portia-Nelly-Mashaba <pnpnelly@gmail.com>
2025-01-25 02:49:29 +0200

    added more information on project description

--- src/components/Archieve.js
+++ src/components/Archieve.js
@@ -30,27 +30,30 @@ const Archieve = () => {
 
         <ArchieveCard
           title="Payment Gateway App"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          desc="A PayStripe gateway app that allows users to make payments with Stripe gateway for a beauty app store. It ensures secure and seamless transactions."
           listItem={["ReactNative", "CSS", "Stripe API", "NodeJS"]}
         />
 
         <ArchieveCard
           title="Audio Recorder App"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          desc="A mobile app for recording voice notes, with features to play, playback, delete, and share recordings over social media. It also includes user login functionality."
           listItem={["ReactNative", "CSS", "REST API"]}
         />
 
+
         <ArchieveCard
           title="MERN Recipe App"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          desc="A full-stack recipe app where users can create, view, update, and delete recipes. It ensures efficient data management and a seamless user experience."
           listItem={["React Js", "NodeJs", "MongoDB"]}
         />
 
-        <ArchieveCard
-          title="Employee App"
-          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          listItem={["React Js", "Firebase", "NodeJs"]}
-        />
+
+      <ArchieveCard
+        title="Employee App"
+        desc="An app with Super Admin and Admin roles for managing employee records, including CRUD operations and secure authentication. It also features real-time updates."
+        listItem={["React Js", "Firebase", "NodeJs"]}
+      />
+
 
         {showMore && (
           <>
@@ -60,10 +63,11 @@ const Archieve = () => {
               transition={{ delay: 0.1 }}
             >
               <ArchieveCard
-                title="Card Guessing Game"
-                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                title="Fruit Card Guessing Game"
+                desc="A fun card guessing game with 18 cards, a timer, and a restart button. Users can test their memory and enjoy a playful experience."
                 listItem={["React Js", "CSS", "NodeJs"]}
               />
+
             </motion.div>
 
             <motion.div
@@ -73,9 +77,10 @@ const Archieve = () => {
             >
               <ArchieveCard
                 title="Book Directory"
-                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                desc="A comprehensive book directory app where users can add, view, update, and delete book entries. It ensures efficient data management."
                 listItem={["NodeJs", "ExpressJs", "REST API"]}
               />
+
             </motion.div>
 
             <motion.div
@@ -83,11 +88,12 @@ const Archieve = () => {
               animate={{ opacity: 1 }}
               transition={{ delay: 0.3 }}
             >
-              <ArchieveCard
+             <ArchieveCard
                 title="Shopping List App"
-                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                desc="A handy app where users can add shopping lists, specify quantities, and perform CRUD operations. It is efficient and convenient."
                 listItem={["NodeJs", "ExpressJs", "REST API"]}
               />
+
             </motion.div>
 
             <motion.div
@@ -96,10 +102,11 @@ const Archieve = () => {
               transition={{ delay: 0.4 }}
             >
               <ArchieveCard
-                title="Quiz Application App"
-                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                listItem={["NodeJs", "ExpressJs"]}
-              />
+  title="Quiz Application App"
+  desc="A command-line quiz app with a timer alert. Users can test their knowledge and enjoy a seamless experience."
+  listItem={["NodeJs", "ExpressJs"]}
+/>
+
             </motion.div>
 
             <motion.div
@@ -107,11 +114,12 @@ const Archieve = () => {
               animate={{ opacity: 1 }}
               transition={{ delay: 0.5 }}
             >
-              <ArchieveCard
-                title="Basic Node.js Server App"
-                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                listItem={["NodeJs", "ExpressJs"]}
-              />
+             <ArchieveCard
+  title="Basic Node.js Server App"
+  desc="A simple server app created using Node.js and Express.js. It handles HTTP requests and responses efficiently."
+  listItem={["NodeJs", "ExpressJs"]}
+/>
+
             </motion.div>
 
             <motion.div
@@ -120,10 +128,12 @@ const Archieve = () => {
               transition={{ delay: 0.6 }}
             >
               <ArchieveCard
-                title="Shopping List App"
-                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                listItem={["NodeJs", "ExpressJs"]}
+                title="News App"
+                desc="A comprehensive news app featuring categories, search functionality, bookmarking, and social media sharing."
+                listItem={["ReactJs", "NewsAPI", "ExpressJs"]}
               />
+
+
             </motion.div>
 
             <motion.div
@@ -132,10 +142,11 @@ const Archieve = () => {
               transition={{ delay: 0.7 }}
             >
               <ArchieveCard
-                title="Employee App"
-                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                listItem={["NodeJs", "ExpressJs"]}
-              />
+  title="Employee App"
+  desc="An app for managing employee records with CRUD operations and search functionality. "
+  listItem={["ReactJs", "Json Server"]}
+/>
+
             </motion.div>
 
             <motion.div
@@ -144,10 +155,11 @@ const Archieve = () => {
               transition={{ delay: 0.8 }}
             >
               <ArchieveCard
-                title=" UI Challenge App"
-                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                listItem={["NodeJs", "ExpressJs", "REST API"]}
-              />
+  title="UI Challenge App"
+  desc="A UI challenge app created using CSS and ReactJs, featuring a beautifully designed card component."
+  listItem={["NodeJs", "CSS"]}
+/>
+
             </motion.div>
 
             <motion.div
@@ -156,10 +168,11 @@ const Archieve = () => {
               transition={{ delay: 0.9 }}
             >
               <ArchieveCard
-                title="Tip Calculator Challenge App"
-                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                listItem={["NodeJs", "ExpressJs"]}
-              />
+  title="Tip Calculator App"
+  desc="A tip calculator app that allows users to split the bill among themselves. It ensures accurate calculations and a seamless user experience."
+  listItem={["ReactJs", "CSS"]}
+/>
+
             </motion.div>
           </>
         )}
